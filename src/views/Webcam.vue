3c36d18db8f77ca55ece85655f676581970c09a2
Simon Genin <simongenin@hotmail.fr>
2019-01-16 09:31:28 +0100

    Working webcamp

--- src/views/Webcam.vue
+++ src/views/Webcam.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
     <div class="flex flex-col justify-center items-center bg-blue-darker shadow rounded p-8 mb-8">
-      <vue-webcam :width="800" ref="webcam" class="mb-8"></vue-webcam>
-    <el-button type="secondary" @click="take_photo()" round>Take the picture</el-button>
+      <vue-webcam :width="800" :height="500" ref="webcam" class="mb-8"></vue-webcam>
+      <el-button type="secondary" @click="take_photo()" round>Take the picture</el-button>
 
-        <hr class="border border-blue-lightest w-full my-12">
+      <hr class="border border-blue-lightest w-full my-12">
 
       <img class="mb-8" :src="photo" alt style="width:600px;height:350px">
-        <el-button type="primary" :loading="working" @click="send()" :disabled="!this.photo" round>Identify user</el-button>
+      <el-button
+        type="primary"
+        :loading="working"
+        @click="send()"
+        :disabled="!this.photo"
+        round
+      >Identify user</el-button>
     </div>
-
   </div>
 </template>
 
 <script>
 import VueWebcam from "../VueWebcam.js";
-import axios from 'axios';
-
+import axios from "axios";
 
 export default {
   components: { VueWebcam },
@@ -24,7 +28,8 @@ export default {
     return {
       photo: null,
       working: false,
-      base_url: ""
+      base_url:
+        "http://humanitarianhack-env.rv9ka63pzi.eu-central-1.elasticbeanstalk.com"
     };
   },
   methods: {
@@ -32,35 +37,32 @@ export default {
       this.photo = this.$refs.webcam.getPhoto();
 
       this.$notify({
-          title: 'Picture',
-          message: 'A picture has been taken',
-          duration: 2000,
-        });
+        title: "Picture",
+        message: "A picture has been taken",
+        duration: 2000
+      });
     },
     send() {
-        this.working = true
-        const encodedString = btoa(this.photo);
+      this.working = true;
+      const encodedString = btoa(this.photo);
 
-        const payload = {
 
-            identifications: {
-
-                image: encodedString
-
-            }
-
-        }
-
-        axios.post(base_url + "/users/authenticate", payload).then( response => {
-            console.log(response.data)
-            this.working = false
-            // this.$store.commit(jwt, response.data.token)
-        }).catch(error => {
-            console.log(error)
-            this.working = false
+      axios
+        .post(this.base_url + "/users/authenticate", {
+          identifications: {
+            image: encodedString
+          }
         })
-
-        
+        .then(response => {
+          console.log(response.data);
+          // this.$store.commit(jwt, response.data.token)
+        })
+        .catch(error => {
+          this.$message.error(error.response.data.error);
+        })
+        .then(() => {
+          this.working = false;
+        });
     }
   }
 };
